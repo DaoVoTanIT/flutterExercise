d45ac83bc8942e15a2cb8954d39e834b399b0eab
Đào Võ Tấn <daovotan3@gmail.com>
2020-10-17 10:34:46 +0700

    Revert "add hello folder"
    This reverts commit 61a2545d9da0771b105f5c298d3d8cb627ca31fa.

--- hello/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:hello/src/home_widget.dart';
-import 'package:hello/src/product.dart';
-
-void main() {
-  runApp(HomeWidget(
-    product: Product('Tester'),
-  ));
-}
